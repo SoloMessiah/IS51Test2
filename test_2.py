5204e71cfd960170f5d941f33f684daaa2cc3f64
SoloMessiah <ch1n0ul33@gmail.com>
2021-04-29 22:51:52 -0700

    Exam-end, all that I know how to do currently.

--- test_2.py
+++ test_2.py
@@ -11,11 +11,6 @@ will print the average grade and the percentage of grades above the average.
 """
 
 
-
-
-
-
-
 """
 
 First funcion, calculate_percent_above_average().
@@ -25,7 +20,7 @@ First funcion, calculate_percent_above_average().
     Return count divided by grade.
 
 Second funtion, main().
-    Set x = open("Final.txt")
+    Set givenFile = open("Final.txt")
     grades = []
 
     Use loop to count number of grades in Final.txt.
@@ -44,3 +39,35 @@ Second funtion, main().
 main
 
 """
+
+
+def calculate_percent_above_average(grades, average):
+
+    count = 0
+    for score in grades:
+        if score > average:
+            count = count + 1
+    return (count / grades)
+
+
+def main():
+
+    givenFile = open("Final.txt")
+    grades = []
+
+    for line in givenFile:
+        grades.append(int(line))
+    print("Number of grades:", grades)
+
+    average = 0
+    for score in grades:
+        average = average + grades
+    average = average / grades
+
+    print("Average grade:", average)
+    print("Percentage of grades above average:")
+
+    givenfile.close()
+
+
+main()
