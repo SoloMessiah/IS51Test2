4cb7557ed169bd7ccad9af6d07b92817600f3a28
SoloMessiah <ch1n0ul33@gmail.com>
2021-04-29 21:43:40 -0700

    Finished structured english.

---
+++ test_2.py
@@ -0,0 +1,10 @@
+"""
+The program will display and calculate the number of grades, the mean grade, and the percentage of grades that are above the average grades. There should be two functions. 
+
+The first function will start with a for loop to count the number of grades, starting at zero, compare it with the average grade, and if the grade is bigger, increase the count.
+Then it will return the count divided by the grades.
+
+The second function, the main function, will open Final.txt file. From here, we will print the number of grades. Afterwards, we will calculate the average grade. Finally, the program 
+will print the average grade and the percentage of grades above the average.
+
+"""
